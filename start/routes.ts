4daa3a8fda2071d3d0d162daf6d430c4a55033a7
Apirak Panatkool <apirakb@gmail.com>
2025-02-16 23:04:03 +0700

    Rendering a view for a route

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,7 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/movies', async (ctx) => {
+    return ctx.view.render('pages/movies',{ movie: 'My lovely movie x'})
+})
